181f622171fad02db60c686abfd578b07e656a7a
josepdemeq <josep@demeq.com>
2021-02-04 18:08:30 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    
+   // Deshabilito el botón derecho del mouse  
+    $(document).bind("contextmenu",function(e){
+        return false;
+     });
+    
+});
+
+
