dab84bb65383d0e5df1495b8ca0c870e1177b906
josepdemeq <josep@demeq.com>
2021-04-20 15:38:25 -0300

    COMMIT

--- formularios/cotizacion.php
+++ formularios/cotizacion.php
@@ -4,18 +4,18 @@
 
         #Reemplazar este correo por el correo electrónico del destinatario
         $mail_to = "josep@demeq.com";
-        $email_subject = "Soporte de producto - WEB";
+        $email_subject = "Cotización - WEB";
         
         # Envío de datos
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $phone = trim($_POST["phone"]);
-        $model = trim($_POST["model"]);
-        $serial = trim($_POST["serial"]);
+        $country = trim($_POST["country"]);
+        $surname = trim($_POST["surname"]);
         $company = trim($_POST["company"]);
         $message = trim($_POST["message"]);
         
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($model) OR empty($serial) OR empty($company) OR empty($message)) {
+        if ( empty($name) OR empty($surname) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($company) OR empty($country) OR empty($message)) {
             # Establecer un código de respuesta y salida.
             http_response_code(400);
             echo "Por favor completa el formulario y vuelve a intentarlo.";
@@ -25,8 +25,7 @@
         # Contenido del correo
         $content = "<html>
                     <body style='font-family:Calibri, Lucida Grande, Arial, sans-serif;max-width:600px;margin:20px auto;'>
-                    <h1 style='color:#000000;font-weight:bold;'> " . $name . " de  " . $company . "  necesita nuestra ayuda con su  " . $model . " </h1>
-                    <h2 style='color:rgb(239,125,45);font-weight:bold;margin-top:0;margin-bottom:0;'>Nos env&iacute;a un mail desde la web para darle soporte</h2>
+                    <h1 style='color:#000000;font-weight:bold;'> " . $name . " " . $surname . " de la empresa  " . $company . " nos env&iacute;a un pedido de cotizaci&oacute;n.</h1>
                     <p style='margin-top:0;margin-bottom:20px;'>A continuaci&oacute;n pod&eacute;s ver su consulta:</p>
                     <div style='border: 1px solid #333;overflow: hidden;width: 100%;max-width: 600px;'>
                         <div style='font-size: 18px;font-weight: 600; color: rgb(239,125,45);width:100%;padding:10px;border-bottom:2px solid rgb(239,125,45);'>
@@ -35,10 +34,12 @@
                         <div style='background: #ffffff;color: #000;width:100%;padding:10px;border-bottom:2px solid #000;font-weight: 600;'>
                             <div style='width: 49%;display: inline-block;margin: 0;'>Nombre y Apellido</div>
                             <div style='width: 49%;display: inline-block;margin: 0;'>Empresa</div>
+                            <div style='width: 49%;display: inline-block;margin: 0;'>Pa&iacute;s</div>
                         </div>
                         <div style='background: #ffffff;;width:100%;padding:10px;border-bottom:1px solid #000000;'>
                             <div style='width: 49%;display: inline-block;margin: 0;'> " . $name . " </div>
                             <div style='width: 49%;display: inline-block;margin: 0;'> " . $company . " </div>
+                            <div style='width: 49%;display: inline-block;margin: 0;'> " . $country . " </div>
                         </div>
                         <div style='background: #ffffff;color: #000;width:100%;padding:10px;border-bottom:2px solid #000;font-weight: 600;'>
                         <div style='width: 49%;display: inline-block;margin: 0;'>Email</div>
@@ -47,19 +48,6 @@
                         <div style='background: #ffffff;;width:100%;padding:10px;'>
                             <div style='width: 49%;display: inline-block;margin: 0;'> " . $email . " </div>
                             <div style='width: 49%;display: inline-block;margin: 0;'> " . $phone . " </div>
-                        </div>
-                    </div>
-                    <div style='border: 1px solid #333;overflow: hidden;width: 100%;max-width: 600px;margin-top: 20px;'>
-                        <div style='font-size: 18px;font-weight: 600; color: rgb(239,125,45);width:100%;padding:10px;border-bottom:2px solid rgb(239,125,45);'>
-                        Datos del equipo
-                        </div>
-                        <div style='background: #ffffff;color: #000;width:100%;padding:10px;border-bottom:2px solid #000;font-weight: 600;'>
-                            <div style='width: 49%;display: inline-block;margin: 0;'>Modelo</div>
-                            <div style='width: 49%;display: inline-block;margin: 0;'>N&uacute;mero de serie</div>
-                        </div>
-                        <div style='background: #ffffff;;width:100%;padding:10px;'>
-                            <div style='width: 49%;display: inline-block;margin: 0;'> " . $model . " </div>
-                            <div style='width: 49%;display: inline-block;margin: 0;'> " . $serial . " </div>
                         </div>
                     </div>
                     <div style='border: 1px solid #333;overflow: hidden;width: 100%;max-width: 600px;margin-top: 20px;'>
@@ -75,7 +63,7 @@
 
 
         # Encabezados de correo electrónico.
-        $headers = "From: $name <$email>";
+        $headers = "From: $name $surname  <$email>";
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=\"UTF-8\"\r\n";  
 
---
+++ js/compra-map.js
@@ -0,0 +1,42 @@
+
+var panZoomMap = svgPanZoom('#map', {
+    controlIconsEnabled: true
+  });
+  // or
+   var svgElement = document.querySelector('#map')
+  var panZoomMap = svgPanZoom('#map', {
+    viewportSelector: '.panzoom'
+  , panEnabled: true
+  , controlIconsEnabled: true
+  , zoomEnabled: true
+  , dblClickZoomEnabled: true
+  , mouseWheelZoomEnabled: false
+  , preventMouseEventsDefault: true
+  , zoomScaleSensitivity: 0.2
+  , minZoom: 0.5
+  , maxZoom: 6
+  , fit: true
+  , contain: false
+  , center: true
+  , refreshRate: 'auto'
+  , beforeZoom: function(){}
+  , onZoom: function(){}
+  , beforePan: function(){}
+  , onPan: function(){}
+  , customEventsHandler: {}
+  , eventsListenerElement: null
+  });
+  
+  
+  // $( "#GL" ).click(function() {
+  //   alert( "You clicked on Greenland" );
+  // });
+  
+  $('#RU').magnificPopup({
+    items: {
+        src: '#test-popup',
+        type: 'inline'
+    },
+     closeBtnInside: true,
+    mainClass: 'mfp-fade'
+  });
